753b14e263d5ffe9ec26a6534f097501bf38eed4
diegorezm <dmourarezende@gmail.com>
2025-03-11 12:53:41 -0300

    main activity with compose navigation

--- app/src/main/java/com/diegorezm/ratemymusic/MainActivity.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/MainActivity.kt
@@ -7,36 +7,66 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.diegorezm.ratemymusic.presentation.auth.GoogleAuthUiClient
+import com.diegorezm.ratemymusic.presentation.auth.sign_in.SignInScreen
+import com.diegorezm.ratemymusic.presentation.auth.sign_in.SignInViewModel
+import com.diegorezm.ratemymusic.presentation.auth.sign_up.SignUpScreen
+import com.diegorezm.ratemymusic.presentation.auth.sign_up.SignUpViewModel
+import com.diegorezm.ratemymusic.presentation.main.MainScreen
 import com.diegorezm.ratemymusic.ui.theme.RateMyMusicTheme
 import com.google.firebase.auth.FirebaseAuth
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.launch
+import kotlinx.serialization.Serializable
 
 class MainActivity : ComponentActivity() {
-    private lateinit var authManager: AuthManager
-    private val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        authManager = AuthManager(firebaseAuth)
         setContent {
             RateMyMusicTheme {
+                var isUserAuthenticated by remember { mutableStateOf(false) }
+
+                DisposableEffect(Unit) {
+                    val authListener = FirebaseAuth.AuthStateListener { auth ->
+                        isUserAuthenticated = auth.currentUser != null
+                    }
+                    FirebaseAuth.getInstance().addAuthStateListener(authListener)
+
+                    // Remove listener when the Composable is destroyed
+                    onDispose {
+                        FirebaseAuth.getInstance().removeAuthStateListener(authListener)
+                    }
+                }
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val isAuthenticated by authManager.isUserAuthenticated.collectAsState()
-                    if (isAuthenticated) {
-                        MainScreen()
-                    } else {
-                        AuthScreen(authManager)
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = if (isUserAuthenticated) MainRouteId else SignInRouteId
+                    ) {
+                        composable<SignInRouteId> {
+                            SignInPage(navController)
+                        }
+                        composable<MainRouteId> {
+                            MainScreen(navController)
+                        }
+                        composable<SignUpRouteId> {
+                            SignUpPage(navController)
+                        }
                     }
                 }
             }
@@ -44,19 +74,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-class AuthManager(private val firebaseAuth: FirebaseAuth) {
+@Serializable
+object SignInRouteId
 
-    private val _isUserAuthenticated = MutableStateFlow<Boolean>(false)
-    val isUserAuthenticated: StateFlow<Boolean> = _isUserAuthenticated
-    private val scope = CoroutineScope(Dispatchers.Main)
+@Composable
+fun SignInPage(navController: NavController) {
+    val context = LocalContext.current
+    val googleAuthUiClient = GoogleAuthUiClient(context)
 
-    init {
-        firebaseAuth.addAuthStateListener { auth ->
-            scope.launch {
-                _isUserAuthenticated.value = auth.currentUser != null
-            }
-
-        }
-    }
+    val signInViewModel = SignInViewModel(googleAuthUiClient)
+    SignInScreen(viewModel = signInViewModel, navController)
 }
+
+@Serializable
+object SignUpRouteId
+
+@Composable
+fun SignUpPage(navController: NavController) {
+    val context = LocalContext.current
+    val googleAuthUiClient = GoogleAuthUiClient(context)
+
+    val signUpViewModel = SignUpViewModel(googleAuthUiClient)
+    SignUpScreen(navController, signUpViewModel)
+}
+
+@Serializable
+object MainRouteId
+
 
