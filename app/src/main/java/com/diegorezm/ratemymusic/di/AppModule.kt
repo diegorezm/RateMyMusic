d22b175ce45cf720b5d2f13db842d3d707f3f906
diegorezm <dmourarezende@gmail.com>
2025-03-15 10:55:05 -0300

    added spotify related repositories

--- app/src/main/java/com/diegorezm/ratemymusic/di/AppModule.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/di/AppModule.kt
@@ -1,14 +1,20 @@
 package com.diegorezm.ratemymusic.di
 
 import android.content.Context
+import com.diegorezm.ratemymusic.modules.music.data.remote.repositories.AlbumsRepository
+import com.diegorezm.ratemymusic.modules.music.domain.repositories.AlbumsRemoteRepository
 import com.diegorezm.ratemymusic.modules.profiles.data_access.ProfileRepository
 import com.diegorezm.ratemymusic.modules.profiles.data_access.ProfileRepositoryImpl
+import com.diegorezm.ratemymusic.modules.spotify_auth.data.local.repositories.SpotifyTokenRepository
+import com.diegorezm.ratemymusic.modules.spotify_auth.domain.repositories.SpotifyTokenLocalRepository
 import com.google.firebase.firestore.FirebaseFirestore
 
 interface AppModule {
     val db: FirebaseFirestore
 
     val profileRepository: ProfileRepository
+    val spotifyTokenRepository: SpotifyTokenRepository
+    val albumsRepository: AlbumsRepository
 }
 
 class AppModuleImpl(context: Context) : AppModule {
@@ -19,5 +25,13 @@ class AppModuleImpl(context: Context) : AppModule {
 
     override val profileRepository: ProfileRepository by lazy {
         ProfileRepositoryImpl(db)
+    }
+
+    override val spotifyTokenRepository: SpotifyTokenRepository by lazy {
+        SpotifyTokenLocalRepository(context)
+    }
+
+    override val albumsRepository: AlbumsRepository by lazy {
+        AlbumsRemoteRepository(context)
     }
 }
