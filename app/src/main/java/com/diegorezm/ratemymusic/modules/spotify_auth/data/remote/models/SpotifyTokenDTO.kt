cb016a65e9bc97c6384ca25bb24e74fc6687ebab
diegorezm <dmourarezende@gmail.com>
2025-03-15 10:56:29 -0300

    remote token DTO

---
+++ app/src/main/java/com/diegorezm/ratemymusic/modules/spotify_auth/data/remote/models/SpotifyTokenDTO.kt
@@ -0,0 +1,24 @@
+package com.diegorezm.ratemymusic.modules.spotify_auth.data.remote.models
+
+import com.diegorezm.ratemymusic.modules.spotify_auth.data.local.entities.SpotifyTokenEntity
+import com.google.gson.annotations.SerializedName
+
+data class SpotifyTokenDTO(
+    @SerializedName("access_token") val accessToken: String,
+    @SerializedName("token_type") val tokenType: String,
+    @SerializedName("expires_in") val expiresIn: Int,
+    @SerializedName("refresh_token") val refreshToken: String,
+    @SerializedName("scope") val scope: String
+)
+
+fun SpotifyTokenDTO.toEntity(): SpotifyTokenEntity {
+    val expiresIn = System.currentTimeMillis() + (this.expiresIn * 1000)
+
+    return SpotifyTokenEntity(
+        accessToken = this.accessToken,
+        tokenType = this.tokenType,
+        expiresIn = expiresIn.toLong(),
+        refreshToken = this.refreshToken,
+        scope = this.scope
+    )
+}
