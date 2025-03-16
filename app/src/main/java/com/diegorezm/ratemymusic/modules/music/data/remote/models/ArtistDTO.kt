45fdc9266cd82fc94222aaedf1e29dbec8aa2999
diegorezm <dmourarezende@gmail.com>
2025-03-16 10:03:28 -0300

    adding fields to artist DTO

--- app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/models/ArtistDTO.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/models/ArtistDTO.kt
@@ -3,10 +3,13 @@ package com.diegorezm.ratemymusic.modules.music.data.remote.models
 import com.google.gson.annotations.SerializedName
 
 data class ArtistDTO(
+    @SerializedName("id") val id: String,
     @SerializedName("external_urls") val externalUrls: ExternalUrlsDTO,
     @SerializedName("href") val href: String,
-    @SerializedName("id") val id: String,
     @SerializedName("name") val name: String,
     @SerializedName("type") val type: String,
-    @SerializedName("uri") val uri: String
+    @SerializedName("uri") val uri: String,
+    @SerializedName("genres") val genres: List<String>,
+    @SerializedName("images") val images: List<ImageDTO>,
+    @SerializedName("popularity") val popularity: Int
 )
