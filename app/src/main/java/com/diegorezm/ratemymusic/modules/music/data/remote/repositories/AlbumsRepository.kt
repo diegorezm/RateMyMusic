d24b8d8b9979b6a1ea910e4833119c178ce7e38d
diegorezm <dmourarezende@gmail.com>
2025-03-16 10:03:37 -0300

    fixing album repository return type

--- app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/repositories/AlbumsRepository.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/repositories/AlbumsRepository.kt
@@ -1,9 +1,10 @@
 package com.diegorezm.ratemymusic.modules.music.data.remote.repositories
 
 import com.diegorezm.ratemymusic.modules.music.domain.models.Album
+import com.diegorezm.ratemymusic.modules.music.domain.models.Tracks
 
 interface AlbumsRepository {
     suspend fun getById(albumId: String, spotifyAuthToken: String? = null): Result<Album>
-    suspend fun getTracks(albumId: String, spotifyAuthToken: String? = null): Result<List<Album>>
+    suspend fun getTracks(albumId: String, spotifyAuthToken: String? = null): Result<Tracks>
     suspend fun getNewReleases(spotifyAuthToken: String? = null): Result<List<Album>>
 }
