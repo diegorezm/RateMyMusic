08386883f90a7012636f9b79e91bcabc168951b5
diegorezm <dmourarezende@gmail.com>
2025-03-16 10:03:11 -0300

    artist mapper

--- app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/mappers/ArtistMapper.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/mappers/ArtistMapper.kt
@@ -1,2 +1,14 @@
 package com.diegorezm.ratemymusic.modules.music.data.remote.mappers
 
+import com.diegorezm.ratemymusic.modules.music.data.remote.models.ArtistDTO
+import com.diegorezm.ratemymusic.modules.music.domain.models.Artist
+
+fun ArtistDTO.toDomain(): Artist {
+    return Artist(
+        name = this.name,
+        id = this.id,
+        externalUrl = this.externalUrls.spotify,
+        imageURL = this.images.firstOrNull()?.url ?: "",
+        genres = this.genres
+    )
+}
