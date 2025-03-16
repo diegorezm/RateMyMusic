001942bae3005ac6ad96e3fb0f7d205c157f05df
diegorezm <dmourarezende@gmail.com>
2025-03-16 10:03:18 -0300

    tracks mapper

--- app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/mappers/TracksMapper.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/mappers/TracksMapper.kt
@@ -1,2 +1,15 @@
 package com.diegorezm.ratemymusic.modules.music.data.remote.mappers
 
+import com.diegorezm.ratemymusic.modules.music.data.remote.models.TracksDTO
+import com.diegorezm.ratemymusic.modules.music.domain.models.Tracks
+
+fun TracksDTO.toDomain(): Tracks {
+    return Tracks(
+        href = this.href,
+        limit = this.limit,
+        next = this.next,
+        offset = this.offset,
+        previous = this.previous,
+        items = this.items.map { it.toDomain() }
+    )
+}
