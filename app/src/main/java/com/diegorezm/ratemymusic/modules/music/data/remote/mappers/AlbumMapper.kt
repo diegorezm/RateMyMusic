d9341faa1475ea86a97fdf4f9f3b02c69bd7e43d
diegorezm <dmourarezende@gmail.com>
2025-03-16 10:03:06 -0300

    adding tracks to album model

--- app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/mappers/AlbumMapper.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/modules/music/data/remote/mappers/AlbumMapper.kt
@@ -4,6 +4,8 @@ import com.diegorezm.ratemymusic.modules.music.data.remote.models.AlbumDTO
 import com.diegorezm.ratemymusic.modules.music.domain.models.Album
 
 fun AlbumDTO.toDomain(): Album {
+    val largestHeightImage = this.images.maxByOrNull { it.height }
+
     return Album(
         id = id,
         name = name,
@@ -11,6 +13,8 @@ fun AlbumDTO.toDomain(): Album {
         releaseDate = releaseDate,
         genres = genres,
         label = label,
-        totalTracks = totalTracks
+        totalTracks = totalTracks,
+        imageURL = largestHeightImage?.url,
+        tracks = this.tracks.toDomain()
     )
 }
