680d1e51e1103e812e53d2338c7dc6bbe7d416c2
diegorezm <dmourarezende@gmail.com>
2025-03-16 10:05:57 -0300

    adding transitions

--- app/src/main/java/com/diegorezm/ratemymusic/MainActivity.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/MainActivity.kt
@@ -4,6 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -12,11 +18,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.diegorezm.ratemymusic.RateMyMusicApp.Companion.appModule
+import com.diegorezm.ratemymusic.presentation.album.AlbumScreen
+import com.diegorezm.ratemymusic.presentation.album.AlbumViewModel
 import com.diegorezm.ratemymusic.presentation.auth.GoogleAuthUiClient
 import com.diegorezm.ratemymusic.presentation.auth.sign_in.SignInScreen
 import com.diegorezm.ratemymusic.presentation.auth.sign_in.SignInViewModel
@@ -33,6 +43,7 @@ class MainActivity : ComponentActivity() {
     lateinit var signInViewModel: SignInViewModel
     lateinit var signUpViewModel: SignUpViewModel
     lateinit var mainViewModel: MainViewModel
+    lateinit var albumViewModel: AlbumViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         googleAuthUiClient = GoogleAuthUiClient(this)
@@ -43,6 +54,7 @@ class MainActivity : ComponentActivity() {
             SignUpViewModel(googleAuthUiClient, appModule.profileRepository)
 
         mainViewModel = MainViewModel(appModule)
+
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -69,7 +81,15 @@ class MainActivity : ComponentActivity() {
                     val navController = rememberNavController()
                     NavHost(
                         navController = navController,
-                        startDestination = if (isUserAuthenticated) MainRouteId else SignInRouteId
+                        enterTransition = {
+                            slideInVertically(
+                                initialOffsetY = { -40 }
+                            ) + expandVertically(
+                                expandFrom = Alignment.Top
+                            ) + fadeIn(initialAlpha = 0.3f)
+                        },
+                        exitTransition = { slideOutVertically() + shrinkVertically() + fadeOut() },
+                        startDestination = if (isUserAuthenticated) MainRouteId else SignInRouteId,
                     ) {
                         composable<SignInRouteId> {
                             SignInScreen(viewModel = signInViewModel, navController)
@@ -79,6 +99,16 @@ class MainActivity : ComponentActivity() {
                         }
                         composable<MainRouteId> {
                             MainScreen(navController, mainViewModel)
+                        }
+                        composable<AlbumRouteId> {
+                            val args = it.toRoute<AlbumRouteId>()
+                            albumViewModel =
+                                AlbumViewModel(
+                                    appModule.albumsRepository,
+                                    appModule.spotifyTokenRepository,
+                                    args.albumId
+                                )
+                            AlbumScreen(navController, albumViewModel)
                         }
                     }
                 }
@@ -95,3 +125,6 @@ object SignUpRouteId
 
 @Serializable
 object MainRouteId
+
+@Serializable
+data class AlbumRouteId(val albumId: String)
