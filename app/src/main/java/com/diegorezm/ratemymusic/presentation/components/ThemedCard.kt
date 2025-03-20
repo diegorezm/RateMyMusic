0b9397e4fadf2d9848d878819ac0d6af38d612c1
diegorezm <dmourarezende@gmail.com>
2025-03-20 18:08:21 -0300

    themed card component

---
+++ app/src/main/java/com/diegorezm/ratemymusic/presentation/components/ThemedCard.kt
@@ -0,0 +1,25 @@
+package com.diegorezm.ratemymusic.presentation.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ThemedCard(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    Card(
+        modifier = modifier.fillMaxWidth(),
+        shape = MaterialTheme.shapes.medium,
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.onBackground
+        )
+    ) {
+        content()
+    }
+}
