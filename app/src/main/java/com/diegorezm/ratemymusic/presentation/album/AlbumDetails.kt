f8bd6e90d89db644b6b996d235a1480764b5bc10
diegorezm <dmourarezende@gmail.com>
2025-03-21 00:27:18 -0300

    spotify button is now a component

--- app/src/main/java/com/diegorezm/ratemymusic/presentation/album/AlbumDetails.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/presentation/album/AlbumDetails.kt
@@ -1,8 +1,6 @@
 package com.diegorezm.ratemymusic.presentation.album
 
-import android.content.Intent
 import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
@@ -13,7 +11,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -22,7 +19,6 @@ import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
@@ -38,21 +34,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.net.toUri
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
-import com.diegorezm.ratemymusic.R
 import com.diegorezm.ratemymusic.modules.music.domain.models.Album
+import com.diegorezm.ratemymusic.presentation.components.SpotifyButton
 import com.diegorezm.ratemymusic.presentation.reviews.ReviewsViewModel
 import com.diegorezm.ratemymusic.utils.formatTimestamp
 
-// This component is doing a lot of things right now
-// i should probably do something about it
 @Composable
 fun AlbumDetail(
     navController: NavController,
@@ -62,7 +53,6 @@ fun AlbumDetail(
 ) {
 
     val formattedDate = formatTimestamp(album.releaseDate)
-    val context = LocalContext.current
     val isFavorite by viewModel.isFavorite.collectAsState()
 
     var selectedTabIndex by remember { mutableIntStateOf(0) }
@@ -73,7 +63,6 @@ fun AlbumDetail(
             .verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        // Album Cover
         AsyncImage(
             model = album.imageURL,
             contentDescription = "Album Cover",
@@ -86,25 +75,21 @@ fun AlbumDetail(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Album Name
         Text(
             text = album.name,
             fontSize = 22.sp,
             fontWeight = FontWeight.Bold
         )
 
-        // Album Label
         Text(
             text = album.label,
             fontSize = 14.sp,
             color = MaterialTheme.colorScheme.primary
         )
 
-        // Release Date
         Text(text = formattedDate, fontSize = 14.sp)
         Text(text = "Total tracks: ${album.totalTracks}", fontSize = 14.sp)
 
-        // Genres
         if (album.genres.isNotEmpty()) {
             Text(
                 text = "Genres: ${album.genres.joinToString(", ")}",
@@ -115,28 +100,7 @@ fun AlbumDetail(
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedButton(
-            onClick = {
-                val intent = Intent(Intent.ACTION_VIEW, album.externalUrl.toUri())
-                context.startActivity(intent)
-            },
-            modifier = Modifier.fillMaxWidth(),
-            shape = MaterialTheme.shapes.medium
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center,
-                modifier = Modifier.padding(4.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.spotify_logo),
-                    contentDescription = "Spotify Logo",
-                    modifier = Modifier.size(24.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(text = "Listen on Spotify", fontWeight = FontWeight.Bold)
-            }
-        }
+        SpotifyButton(externalURL = album.externalUrl)
 
         Spacer(modifier = Modifier.height(16.dp))
 
