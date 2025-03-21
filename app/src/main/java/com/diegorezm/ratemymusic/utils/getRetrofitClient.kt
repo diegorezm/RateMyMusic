7e7df52e44eb6a93f8152e7e882208a363922a2b
diegorezm <dmourarezende@gmail.com>
2025-03-21 16:17:10 -0300

    retrofit

--- app/src/main/java/com/diegorezm/ratemymusic/utils/getRetrofitClient.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/utils/getRetrofitClient.kt
@@ -1,10 +1,15 @@
 package com.diegorezm.ratemymusic.utils
 
 import android.content.Context
+import android.util.Log
 import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
+import okhttp3.ResponseBody.Companion.toResponseBody
 import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONException
+import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
@@ -17,7 +22,7 @@ object RetrofitClient {
         var request = chain.request()
         if (!isNetworkAvailable(context)) {
             request = request.newBuilder()
-                .header("Cache-Control", "public, only-if-cached, max-stale=86400") // 1 day offline
+                .header("Cache-Control", "public, only-if-cached, max-stale=86400")
                 .build()
         }
         chain.proceed(request)
@@ -26,7 +31,7 @@ object RetrofitClient {
     private fun cacheInterceptor() = Interceptor { chain ->
         val response = chain.proceed(chain.request())
         response.newBuilder()
-            .header("Cache-Control", "public, max-age=${MAX_CACHE_AGE * 24 * 60 * 60}") // 1 day
+            .header("Cache-Control", "public, max-age=${MAX_CACHE_AGE * 24 * 60 * 60}")
             .build()
     }
 
@@ -53,5 +58,32 @@ object RetrofitClient {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as android.net.ConnectivityManager
         return connectivityManager.activeNetworkInfo?.isConnectedOrConnecting == true
+    }
+}
+
+class PrettyJsonLoggingInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val response = chain.proceed(request)
+        val responseBody = response.body
+
+        responseBody?.let {
+            val contentType = it.contentType()
+            val rawJson = it.string()
+
+            if (contentType?.subtype == "json") {
+                try {
+                    val prettyJson = JSONObject(rawJson).toString(4)
+                    Log.d("PrettyJson", prettyJson)
+                } catch (e: JSONException) {
+                    Log.d("PrettyJson", "Invalid JSON")
+                }
+            }
+
+            val newResponseBody = rawJson.toResponseBody(contentType)
+            return response.newBuilder().body(newResponseBody).build()
+        }
+
+        return response
     }
 }
