7d555fea2e6aae2f2404ba2adb2c50371c9229e9
diegorezm <dmourarezende@gmail.com>
2025-03-21 00:28:32 -0300

    removing main

--- app/src/main/java/com/diegorezm/ratemymusic/presentation/main/MainViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.diegorezm.ratemymusic.presentation.main
-
-import androidx.lifecycle.ViewModel
-import com.diegorezm.ratemymusic.di.AppModule
-
-class MainViewModel(
-    private val appModule: AppModule
-) : ViewModel() {
-}
--- app/src/main/java/com/diegorezm/ratemymusic/presentation/main/NavItem.kt
+++
@@ -1,8 +0,0 @@
-package com.diegorezm.ratemymusic.presentation.main
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class NavItem(
-    val label: String,
-    val icon: ImageVector
-)
