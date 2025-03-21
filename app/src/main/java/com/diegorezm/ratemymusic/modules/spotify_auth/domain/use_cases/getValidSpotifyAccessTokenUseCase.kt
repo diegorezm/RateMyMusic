dab53028909da8ecf9501b88a44421014c61d34b
diegorezm <dmourarezende@gmail.com>
2025-03-21 16:15:55 -0300

    not a valid token if its not there

--- app/src/main/java/com/diegorezm/ratemymusic/modules/spotify_auth/domain/use_cases/getValidSpotifyAccessTokenUseCase.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/modules/spotify_auth/domain/use_cases/getValidSpotifyAccessTokenUseCase.kt
@@ -3,7 +3,7 @@ package com.diegorezm.ratemymusic.modules.spotify_auth.domain.use_cases
 import android.util.Log
 import com.diegorezm.ratemymusic.modules.spotify_auth.data.local.repositories.SpotifyTokenRepository
 
-suspend fun getValidSpotifyAccessTokenUseCase(repository: SpotifyTokenRepository): Result<String?> {
+suspend fun getValidSpotifyAccessTokenUseCase(repository: SpotifyTokenRepository): Result<String> {
     return try {
         if (repository.isTokenExpired()) {
             val newToken = refreshSpotifyTokenUseCase(repository).getOrNull()
@@ -12,7 +12,11 @@ suspend fun getValidSpotifyAccessTokenUseCase(repository: SpotifyTokenRepository
             }
             Result.success(newToken)
         } else {
-            Result.success(repository.getToken()?.accessToken)
+            val token = repository.getToken()?.accessToken
+            if (token == null) {
+                return Result.failure(Exception("No token found."))
+            }
+            Result.success(token)
         }
     } catch (e: Exception) {
         Log.e("SpotifyToken", "Error getting valid token", e)
