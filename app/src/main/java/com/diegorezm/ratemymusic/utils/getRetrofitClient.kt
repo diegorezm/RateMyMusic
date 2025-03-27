c9bbdbf236965bb59a7db9d9bad6a3411ba9a3df
diegorezm <dmourarezende@gmail.com>
2025-03-27 18:07:40 -0300

    removing logging

--- app/src/main/java/com/diegorezm/ratemymusic/utils/getRetrofitClient.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/utils/getRetrofitClient.kt
@@ -1,15 +1,9 @@
 package com.diegorezm.ratemymusic.utils
 
 import android.content.Context
-import android.util.Log
 import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.Response
-import okhttp3.ResponseBody.Companion.toResponseBody
-import okhttp3.logging.HttpLoggingInterceptor
-import org.json.JSONException
-import org.json.JSONObject
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.File
@@ -42,9 +36,6 @@ object RetrofitClient {
             .cache(cache)
             .addInterceptor(offlineCacheInterceptor(context))
             .addNetworkInterceptor(cacheInterceptor())
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            })
             .build()
 
         return Retrofit.Builder()
@@ -57,33 +48,6 @@ object RetrofitClient {
     private fun isNetworkAvailable(context: Context): Boolean {
         val connectivityManager =
             context.getSystemService(Context.CONNECTIVITY_SERVICE) as android.net.ConnectivityManager
-        return connectivityManager.activeNetworkInfo?.isConnectedOrConnecting == true
-    }
-}
-
-class PrettyJsonLoggingInterceptor : Interceptor {
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val response = chain.proceed(request)
-        val responseBody = response.body
-
-        responseBody?.let {
-            val contentType = it.contentType()
-            val rawJson = it.string()
-
-            if (contentType?.subtype == "json") {
-                try {
-                    val prettyJson = JSONObject(rawJson).toString(4)
-                    Log.d("PrettyJson", prettyJson)
-                } catch (e: JSONException) {
-                    Log.d("PrettyJson", "Invalid JSON")
-                }
-            }
-
-            val newResponseBody = rawJson.toResponseBody(contentType)
-            return response.newBuilder().body(newResponseBody).build()
-        }
-
-        return response
+        return connectivityManager.activeNetwork != null
     }
 }
