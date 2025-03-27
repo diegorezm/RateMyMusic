f54e33f620e07deb8a4c7a811b5ba4951dacfe17
diegorezm <dmourarezende@gmail.com>
2025-03-27 18:07:21 -0300

    scaffold with top bar

--- app/src/main/java/com/diegorezm/ratemymusic/presentation/track/TrackScreen.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/presentation/track/TrackScreen.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
@@ -21,10 +20,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -34,10 +30,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import coil3.compose.AsyncImage
@@ -46,6 +40,7 @@ import com.diegorezm.ratemymusic.modules.music.domain.models.Track
 import com.diegorezm.ratemymusic.presentation.components.BottomDrawer
 import com.diegorezm.ratemymusic.presentation.components.ErrorMessage
 import com.diegorezm.ratemymusic.presentation.components.LoadingIndicator
+import com.diegorezm.ratemymusic.presentation.components.ScaffoldWithTopBar
 import com.diegorezm.ratemymusic.presentation.components.SpotifyButton
 import com.diegorezm.ratemymusic.presentation.reviews.ReviewsScreen
 import com.diegorezm.ratemymusic.presentation.reviews.ReviewsViewModel
@@ -58,48 +53,19 @@ fun TrackScreen(
     reviewsViewModel: ReviewsViewModel
 ) {
     val track by viewModel.trackState.collectAsState()
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(
-                        text = "Track Details",
-                        style = MaterialTheme.typography.titleSmall
-                    )
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.background,
-                    titleContentColor = MaterialTheme.colorScheme.onBackground
-                ),
-                navigationIcon = {
-                    val imageVector =
-                        ImageVector.vectorResource(R.drawable.baseline_keyboard_arrow_left_24)
-                    IconButton(onClick = { navController.navigateUp() }) {
-                        Icon(imageVector = imageVector, contentDescription = "Back")
-                    }
+    ScaffoldWithTopBar(title = "Track Details", navController = navController) {
+        when (track) {
+            is TrackState.Success -> {
+                val trackData = (track as TrackState.Success).track
+                if (trackData == null) {
+                    NoTrackFound()
+                } else {
+                    TrackContent(trackData, viewModel, reviewsViewModel, navController)
                 }
-            )
-        }
-    ) { paddingValues ->
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-        ) {
-            when (track) {
-                is TrackState.Success -> {
-                    val trackData = (track as TrackState.Success).track
-                    if (trackData == null) {
-                        NoTrackFound()
-                    } else {
-                        TrackContent(trackData, viewModel, reviewsViewModel, navController)
-                    }
-                }
-
-                is TrackState.Error -> ErrorMessage((track as TrackState.Error).message)
-                TrackState.Idle, TrackState.Loading -> LoadingIndicator()
             }
+
+            is TrackState.Error -> ErrorMessage((track as TrackState.Error).message)
+            TrackState.Idle, TrackState.Loading -> LoadingIndicator()
         }
     }
 }
