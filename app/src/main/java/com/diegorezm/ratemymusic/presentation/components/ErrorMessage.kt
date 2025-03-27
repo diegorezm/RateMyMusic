e846206161eeb56b08f4b40f0e7ac3edf8a92be4
diegorezm <dmourarezende@gmail.com>
2025-03-27 18:06:54 -0300

    error message component

--- app/src/main/java/com/diegorezm/ratemymusic/presentation/components/ErrorMessage.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/presentation/components/ErrorMessage.kt
@@ -1,20 +1,39 @@
 package com.diegorezm.ratemymusic.presentation.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.diegorezm.ratemymusic.ui.theme.RateMyMusicTheme
 
 @Composable
 fun ErrorMessage(message: String) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(60.dp)
+            .background(MaterialTheme.colorScheme.error, MaterialTheme.shapes.medium),
+        contentAlignment = Alignment.Center
+    ) {
         Text(
             text = message,
-            color = MaterialTheme.colorScheme.error,
+            color = MaterialTheme.colorScheme.onError,
             style = MaterialTheme.typography.bodyLarge
         )
+    }
+}
+
+@Preview
+@Composable
+private fun ErrorMessagePreview() {
+    RateMyMusicTheme {
+        ErrorMessage("An error occurred")
     }
 }
