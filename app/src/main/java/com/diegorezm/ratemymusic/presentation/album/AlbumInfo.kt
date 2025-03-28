42e9ee916f0d32f22bb471307cfb74e4cdd25b28
diegorezm <dmourarezende@gmail.com>
2025-03-28 10:59:01 -0300

    better album info

--- app/src/main/java/com/diegorezm/ratemymusic/presentation/album/AlbumInfo.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/presentation/album/AlbumInfo.kt
@@ -24,6 +24,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
@@ -67,16 +68,29 @@ fun AlbumInfo(
         Text(
             text = album.name,
             fontSize = 22.sp,
-            fontWeight = FontWeight.Bold
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
         )
 
-        Text(text = formattedDate, fontSize = 14.sp)
+        Text(
+            text = album.artists.joinToString(", ") { it.name },
+            fontSize = 14.sp,
+            color = MaterialTheme.colorScheme.onSurface,
+            textAlign = TextAlign.Center
+
+        )
+
+        Text(
+            text = formattedDate, fontSize = 14.sp, textAlign = TextAlign.Center
+        )
 
         if (album.genres.isNotEmpty()) {
             Text(
                 text = "Genres: ${album.genres.joinToString(", ")}",
                 fontSize = 14.sp,
-                modifier = Modifier.padding(top = 8.dp)
+                modifier = Modifier.padding(top = 8.dp),
+                textAlign = TextAlign.Center
+
             )
         }
 
