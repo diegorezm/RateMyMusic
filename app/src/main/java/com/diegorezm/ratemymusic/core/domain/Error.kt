268ac6c24e3ed5e9d5758aa072dce524427113f7
diegorezm <dmourarezende@gmail.com>
2025-04-04 16:58:56 -0300

    core

---
+++ app/src/main/java/com/diegorezm/ratemymusic/core/domain/Error.kt
@@ -0,0 +1,3 @@
+package com.diegorezm.ratemymusic.core.domain
+
+interface Error
---
+++ app/src/main/java/com/diegorezm/ratemymusic/core/presentation/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.diegorezm.ratemymusic.core.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Background = Color(0xFFEDE4D1)
+val onBackground = Color(0xFF282B2E)
+
+val Primary = Color(0xFF282B2E)
+val OnPrimary = Color(0xFFEDE4D1)
+
+val Secondary = Color(0xFFCFDE67)
+val OnSecondary = Color(0xFF282B2E)
+
+val Tertiary = Color(0xFFC889BB)
+val OnTertiary = Color(0xFFF0ECE7)
+
+val Surface = Color(0xFFF0ECE7)
+val OnSurface = Color(0xFF282B2E)
+
+val Error = Color(0xFFD81E5B)
+val OnError = Color(0xFF000000)
