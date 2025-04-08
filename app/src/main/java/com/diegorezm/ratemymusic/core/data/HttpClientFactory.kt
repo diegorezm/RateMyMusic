a974aca1cca76e7f5b29d3421417bb918cb658de
diegorezm <dmourarezende@gmail.com>
2025-04-08 00:21:50 -0300

    imports

--- app/src/main/java/com/diegorezm/ratemymusic/core/data/HttpClientFactory.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/core/data/HttpClientFactory.kt
@@ -1,18 +1,17 @@
 package com.diegorezm.ratemymusic.core.data
 
-import android.R.attr.level
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.HttpTimeout
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.http.ContentType
 import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import io.ktor.client.plugins.logging.Logger
-import io.ktor.http.ContentType
 
 object HttpClientFactory {
 
