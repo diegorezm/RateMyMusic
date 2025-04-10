bf2c7a179966bc3a99d94ebdfa5e120fce876b30
diegorezm <dmourarezende@gmail.com>
2025-04-10 15:05:35 -0300

    adding user favorites

--- app/src/main/java/com/diegorezm/ratemymusic/profile/presentation/ProfileScreen.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/profile/presentation/ProfileScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,19 +12,36 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.navigation.NavController
 import com.diegorezm.ratemymusic.core.presentation.components.LoadingIndicator
 import com.diegorezm.ratemymusic.core.presentation.toUiText
 import com.diegorezm.ratemymusic.profile.presentation.components.ProfileCard
 import com.diegorezm.ratemymusic.profile.presentation.components.ProfileFollowersCount
+import com.diegorezm.ratemymusic.user_favorites.presentation.UserFavoritesScreenRoot
+import com.diegorezm.ratemymusic.user_favorites.presentation.UserFavoritesViewModel
 
 @Composable
 fun ProfileScreenRoot(
     modifier: Modifier = Modifier,
     viewModel: ProfileViewModel,
+    userFavoritesViewModel: UserFavoritesViewModel,
+    navController: NavController
 ) {
     val state by viewModel.state.collectAsStateWithLifecycle()
-    ProfileScreen(modifier.padding(16.dp), state)
-
+    LazyColumn {
+        item {
+            ProfileScreen(modifier.padding(16.dp), state)
+        }
+        item {
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        item {
+            UserFavoritesScreenRoot(
+                viewModel = userFavoritesViewModel,
+                navController = navController
+            )
+        }
+    }
 }
 
 @Composable
