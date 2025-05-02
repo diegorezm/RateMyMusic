847175423688705ec08837bc5fbf6dc0016320c4
diegorezm <dmourarezende@gmail.com>
2025-05-02 09:18:34 -0300

    preparing for cache

--- app/src/main/java/com/diegorezm/ratemymusic/profile/presentation/ProfileViewModel.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/profile/presentation/ProfileViewModel.kt
@@ -56,7 +56,7 @@ class ProfileViewModel(
     }
 
 
-    fun fetchFollowsAndFollowers() {
+    fun fetchFollowsAndFollowers(noCache: Boolean = false) {
         viewModelScope.launch {
             followersRepository.getFollowersCount(profileId).onSuccess {
                 _state.value = _state.value.copy(followersCount = it)
--- app/src/main/java/com/diegorezm/ratemymusic/user_favorites/presentation/UserFavoritesViewModel.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/user_favorites/presentation/UserFavoritesViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.diegorezm.ratemymusic.core.domain.onError
 import com.diegorezm.ratemymusic.core.domain.onSuccess
+import com.diegorezm.ratemymusic.music.albums.domain.Album
 import com.diegorezm.ratemymusic.music.albums.domain.AlbumsRepository
+import com.diegorezm.ratemymusic.music.artists.domain.Artist
 import com.diegorezm.ratemymusic.music.artists.domain.ArtistRepository
+import com.diegorezm.ratemymusic.music.tracks.domain.Track
 import com.diegorezm.ratemymusic.music.tracks.domain.TracksRepository
 import com.diegorezm.ratemymusic.user_favorites.domain.UserFavoritesRepository
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -22,6 +25,9 @@ class UserFavoritesViewModel(
     val profileId: String
 ) : ViewModel() {
     private val _state = MutableStateFlow(UserFavoritesState())
+    private val _cachedAlbums = MutableStateFlow<List<Album>>(emptyList())
+    private val _cachedTracks = MutableStateFlow<List<Track>>(emptyList())
+    private val _cachedArtists = MutableStateFlow<List<Artist>>(emptyList())
     val state = _state
         .onStart {
             fetchUserFavorites()
@@ -32,26 +38,29 @@ class UserFavoritesViewModel(
         )
 
     fun onAction(action: UserFavoritesScreenActions) {
-        // just in case i need to add more actions, but for now this is useless
         when (action) {
+            is UserFavoritesScreenActions.OnRefreshRequested -> {
+                fetchUserFavorites(true)
+            }
+
             else -> Unit
         }
     }
 
-    fun fetchUserFavorites() {
+    fun fetchUserFavorites(noCache: Boolean = false) {
         _state.value = _state.value.copy(isLoading = true)
         viewModelScope.launch {
             userFavoritesRepository.getUserFavorites(profileId).onSuccess {
                 _state.value = _state.value.copy(isLoading = false)
                 if (it.tracks.isNotEmpty()) {
-                    fetchTracks(it.tracks.map { it.entityId })
+                    fetchTracks(it.tracks.map { it.entityId }, noCache)
                 }
 
                 if (it.albums.isNotEmpty()) {
-                    fetchAlbums(it.albums.map { it.entityId })
+                    fetchAlbums(it.albums.map { it.entityId }, noCache)
                 }
                 if (it.artists.isNotEmpty()) {
-                    fetchArtists(it.artists.map { it.entityId })
+                    fetchArtists(it.artists.map { it.entityId }, noCache)
                 }
             }.onError {
                 _state.value = _state.value.copy(isLoading = false, error = it)
@@ -60,22 +69,32 @@ class UserFavoritesViewModel(
 
     }
 
-    fun fetchAlbums(ids: List<String>) {
-        _state.value = _state.value.copy(isLoading = true)
+    fun fetchAlbums(ids: List<String>, noCache: Boolean = false) {
+        if (noCache == false && _cachedAlbums.value.isNotEmpty()) {
+            _state.value = _state.value.copy(albums = _cachedAlbums.value)
+            return
+        }
         viewModelScope.launch {
+            _state.value = _state.value.copy(isLoading = true)
             albumRepository.getAlbumsByIds(ids).onSuccess {
                 _state.value = _state.value.copy(isLoading = false, albums = it)
+                _cachedAlbums.value = it
             }.onError {
                 _state.value = _state.value.copy(isLoading = false, error = it)
             }
         }
     }
 
-    fun fetchTracks(ids: List<String>) {
-        _state.value = _state.value.copy(isLoading = true)
+    fun fetchTracks(ids: List<String>, noCache: Boolean = false) {
+        if (noCache == false && _cachedTracks.value.isNotEmpty()) {
+            _state.value = _state.value.copy(tracks = _cachedTracks.value)
+            return
+        }
         viewModelScope.launch {
+            _state.value = _state.value.copy(isLoading = true)
             trackRepository.getTracksByIds(ids).onSuccess {
                 _state.value = _state.value.copy(isLoading = false, tracks = it)
+                _cachedTracks.value = it
             }
                 .onError {
                     _state.value = _state.value.copy(isLoading = false, error = it)
@@ -84,12 +103,18 @@ class UserFavoritesViewModel(
     }
 
 
-    fun fetchArtists(ids: List<String>) {
-        _state.value = _state.value.copy(isLoading = true)
+    fun fetchArtists(ids: List<String>, noCache: Boolean = false) {
+        if (noCache == false && _cachedArtists.value.isNotEmpty()) {
+            _state.value = _state.value.copy(artists = _cachedArtists.value)
+            return
+        }
 
         viewModelScope.launch {
+            _state.value = _state.value.copy(isLoading = true)
+
             artistRepository.getArtistsByIds(ids).onSuccess {
                 _state.value = _state.value.copy(isLoading = false, artists = it)
+                _cachedArtists.value = it
             }.onError {
                 _state.value = _state.value.copy(isLoading = false, error = it)
             }
