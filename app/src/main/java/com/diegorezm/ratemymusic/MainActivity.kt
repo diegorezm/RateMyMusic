ca23993d0d8b6d4b76a2c950ad3d4ed2aa6809ae
diegorezm <dmourarezende@email.com>
2025-05-21 00:16:32 -0300

    initializing ad s

--- app/src/main/java/com/diegorezm/ratemymusic/MainActivity.kt
+++ app/src/main/java/com/diegorezm/ratemymusic/MainActivity.kt
@@ -5,10 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.diegorezm.ratemymusic.app.App
+import com.google.android.gms.ads.MobileAds
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        MobileAds.initialize(this) { }
         enableEdgeToEdge()
         setContent {
             App()
